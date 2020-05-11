0b2b135e490188f39ce3f2724233005786ef6d74
Melvin Davis <melvinodsa@gmail.com>
2020-05-11 09:12:14 +0530

    Updates

---
+++ precache-manifest.8dc0e297e513f179da859d3cfd37b283.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1f32c43c19798dc12d0029197b7b94",
+    "url": "/chart-comparisons/index.html"
+  },
+  {
+    "revision": "48b3a74b8d87c1244565",
+    "url": "/chart-comparisons/static/css/main.4a977734.chunk.css"
+  },
+  {
+    "revision": "62b9a8fe8289dc0c56ac",
+    "url": "/chart-comparisons/static/js/3.3a18d163.chunk.js"
+  },
+  {
+    "revision": "7c91597dbe6f9b3af652d6c52b278048",
+    "url": "/chart-comparisons/static/js/3.3a18d163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbc5a0a0c9e02865aee2",
+    "url": "/chart-comparisons/static/js/4.3ffca5d1.chunk.js"
+  },
+  {
+    "revision": "c799d0b2b508508abff6",
+    "url": "/chart-comparisons/static/js/5.a6eef2ca.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/chart-comparisons/static/js/5.a6eef2ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b44ffd859829df7261b",
+    "url": "/chart-comparisons/static/js/6.6a62edfe.chunk.js"
+  },
+  {
+    "revision": "48b3a74b8d87c1244565",
+    "url": "/chart-comparisons/static/js/main.cf0695ab.chunk.js"
+  },
+  {
+    "revision": "38cf2c1bd1cd53153e47",
+    "url": "/chart-comparisons/static/js/runtime-main.21945be4.js"
+  },
+  {
+    "revision": "69bb14437fc3b545ec2a",
+    "url": "/chart-comparisons/static/js/xlsx.0acf7d8d.chunk.js"
+  }
+]);
